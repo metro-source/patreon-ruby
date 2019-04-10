f3bb6830b30c0d7773f007b2511e96e221582070
Hernan <hernan@null.net>
2019-04-10 19:36:34 -0400

    Switch API to v2

--- lib/patreon/api.rb
+++ lib/patreon/api.rb
@@ -4,27 +4,17 @@ module Patreon
       @access_token = access_token
     end
 
-    def fetch_user(opts = {})
-      get_parse_json(Utils::JSONAPI::URLUtil.build_url('current_user', opts[:includes], opts[:fields]))
+    def fetch_identity(opts = {})
+      get_json(Utils::JSONAPI::URLUtil.build_url('identity',  opts[:includes], opts[:fields]))
     end
 
-    def fetch_campaign(opts = {})
-      get_parse_json(Utils::JSONAPI::URLUtil.build_url('current_user/campaigns', opts[:includes], opts[:fields]))
-    end
+    # def fetch_user(opts = {})
+    #   get_parse_json(Utils::JSONAPI::URLUtil.build_url('current_user', opts[:includes], opts[:fields]))
+    # end
 
-    def fetch_campaign_and_patrons(opts = {})
-      opts[:includes] = opts[:includes] ? Array(opts[:includes]) : []
-      opts[:includes].concat(Schemas::Campaign.default_relationships + [Schemas::Campaign::Relationships::PLEDGES])
-      fetch_campaign(opts)
-    end
-
-    def fetch_page_of_pledges(campaign_id, opts = {})
-      params = {}
-      params["page[count]"] = opts[:count] || 10
-      params["page[cursor]"] = opts[:cursor] if opts[:cursor]
-      url = "campaigns/#{campaign_id}/pledges?#{Rack::Utils.build_query(params)}"
-      get_parse_json(Patreon::Utils::JSONAPI::URLUtil.build_url(url, opts[:includes], opts[:fields]))
-    end
+    # def fetch_campaign(opts = {})
+    #   get_parse_json(Utils::JSONAPI::URLUtil.build_url('current_user/campaigns', opts[:includes], opts[:fields]))
+    # end
 
     private
 
@@ -37,13 +27,7 @@ module Patreon
       http = Net::HTTP.new("www.patreon.com", 443)
       http.use_ssl = true
 
-      # TODO: It would be nice if we verified our certs
-      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-
-      #SECURITY HOLE
-      http.set_debug_output($stdout) if ENV['DEBUG']
-
-      req = Net::HTTP::Get.new("/api/oauth2/api/#{suffix}")
+      req = Net::HTTP::Get.new("/api/oauth2/v2/#{suffix}")
       req['Authorization'] = "Bearer #{@access_token}"
       req['User-Agent'] = Utils::Client.user_agent_string
       http.request(req).body
